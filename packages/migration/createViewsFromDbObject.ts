bd0d1884aaa021ae71269fcaac6173ba35638a3c
dustin-H <dustin.hoffner@gmail.com>
2017-12-12 00:22:55 +0100

    Revoke table access

--- packages/migration/createViewsFromDbObject.ts
+++ packages/migration/createViewsFromDbObject.ts
@@ -9,6 +9,12 @@ const operationMapper = {
 export default (dbObject, applicationUserName) => {
   const statements = [];
 
+  Object.values(dbObject.tables).forEach((table) => {
+    if (table.isDbModel === true) {
+      statements.push(`REVOKE ALL PRIVILEGES ON "${table.name}" FROM ${applicationUserName};`);
+    }
+  });
+
   Object.values(dbObject.views).forEach((view) => {
     let security = '';
     const fieldSelects = view.fields.map((field) => {
@@ -19,11 +25,11 @@ export default (dbObject, applicationUserName) => {
       security = ' WITH (security_barrier)';
     }
 
-    statements.push(`DROP VIEW ${view.name} IF EXISTS;`);
-    statements.push(`CREATE VIEW "${view.name}"${security} AS SELECT ${fieldSelects.join(', ')} FROM ${view.tableName};`);
+    statements.push(`DROP VIEW "${view.name}" IF EXISTS;`);
+    statements.push(`CREATE VIEW "${view.name}"${security} AS SELECT ${fieldSelects.join(', ')} FROM "${view.tableName}";`);
 
-    statements.push(`REVOKE ALL PRIVILEGES ON ${view.name} FROM ${applicationUserName};`);
-    statements.push(`GRANT ${operationMapper[view.operation]} ON ${view.name} TO ${applicationUserName};`);
+    statements.push(`REVOKE ALL PRIVILEGES ON "${view.name}" FROM ${applicationUserName};`);
+    statements.push(`GRANT ${operationMapper[view.operation]} ON "${view.name}" TO ${applicationUserName};`);
   });
 
   return statements;
