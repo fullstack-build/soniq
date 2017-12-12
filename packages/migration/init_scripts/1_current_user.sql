b46d0c8b87901d7c70e583fdc7233979643de635
dustin-H <dustin.hoffner@gmail.com>
2017-12-12 18:38:46 +0100

    Update current_user() function

--- packages/migration/init_scripts/1_current_user.sql
+++ packages/migration/init_scripts/1_current_user.sql
@@ -1,26 +1,14 @@
-CREATE FUNCTION current_user() RETURNS text AS $$
-DECLARE
-    v_key   TEXT;
-    v_parts TEXT[];
-    v_uname TEXT;
-    v_value TEXT;
-    v_timestamp INT;
-    v_signature TEXT;
-BEGIN
+CREATE OR REPLACE FUNCTION public.current_user()
+    RETURNS "User"
+    LANGUAGE 'sql'
 
-    -- no password verification this time
-    SELECT sign_key INTO v_key FROM secrets;
+    COST 100
+    STABLE
+AS 
+$BODY$
 
-    v_parts := regexp_split_to_array(current_setting('my.username', true), ':');
-    v_uname := v_parts[1];
-    v_timestamp := v_parts[2];
-    v_signature := v_parts[3];
+  select *
+  from public."User"
+  where id = current_setting('jwt.claims.user_id')::uuid
 
-    v_value := v_uname || ':' || v_timestamp || ':' || v_key;
-    IF v_signature = crypt(v_value, v_signature) THEN
-        RETURN v_uname;
-    END IF;
-
-    RAISE EXCEPTION 'invalid username / timestamp';
-END;
-$$ LANGUAGE plpgsql SECURITY DEFINER STABLE;
+$BODY$;
