5b436dadeb0f0f1e8fbd2d5a37c1090cddea2add
dustin-H <dustin.hoffner@gmail.com>
2018-03-14 13:53:53 +0100

    fix gql pkg

---
+++ dev.sh
@@ -0,0 +1,9 @@
+npm install
+sh pkg.sh
+lerna link --force-local
+cd examples/fullstack-one-example
+npm link @fullstack-one/di
+npm link @fullstack-one/graphql
+npm link @fullstack-one/auto-migrate
+npm link @fullstack-one/auth
+npm link fullstack-one
