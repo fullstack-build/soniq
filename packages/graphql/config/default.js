52b475352740f73f91c8e08aa34cd65420788092
dustin-H <dustin.hoffner@gmail.com>
2018-09-06 22:23:52 +0200

    Costcalculator fix

--- packages/graphql/config/default.js
+++ packages/graphql/config/default.js
@@ -3,6 +3,7 @@ module.exports = {
     endpoint:               '/graphql',
     graphiQlEndpointActive: true,
     graphiQlEndpoint:       '/graphiql',
-    queryCostLimit:         2000000
+    queryCostLimit:         2000000,
+    minQueryDepthToCheckCostLimit: 3
   }
 };
