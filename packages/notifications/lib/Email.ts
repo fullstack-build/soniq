4932e41f7dc05941d13aa179f59cfc24335db869
Eugene Terehov <opensource@terehov.de>
2018-09-14 21:30:08 +0200

    Notifications: Added Mailgen dependency for templated mails in Email

--- packages/notifications/lib/Email.ts
+++ packages/notifications/lib/Email.ts
@@ -1,5 +1,6 @@
 import { createTestAccount, createTransport, getTestMessageUrl } from 'nodemailer';
 import { htmlToText } from 'nodemailer-html-to-text';
+import * as Mailgen from 'mailgen';
 
 import { QueueFactory } from '@fullstack-one/queue';
 import { Service, Inject, Container } from '@fullstack-one/di';
@@ -15,15 +16,15 @@ export class Email {
   private isReady = false;
   private transport;
   private readonly queueName = 'notifications.Email';
+  private readonly mailGenerator: Mailgen;
 
   // DI dependencies
   private readonly config: Config;
   private readonly CONFIG: any;
   private readonly logger: ILogger;
+  private readonly queueFactory: QueueFactory;
   @Inject()
   private readonly eventEmitter: EventEmitter;
-
-  private readonly queueFactory: QueueFactory;
 
   constructor(
     @Inject(type => LoggerFactory) loggerFactory,
@@ -37,9 +38,12 @@ export class Email {
     this.config = config;
     // register package config
     this.config.registerConfig('Notifications', __dirname + '/../config');
+    this.CONFIG = this.config.getConfig('Notifications').Email;
 
     this.logger = loggerFactory.create(this.constructor.name);
-    this.CONFIG = this.config.getConfig('Notifications').Email;
+
+    // create Mailgen
+    this.mailGenerator = new Mailgen(this.CONFIG.mailgen);
 
     // add migration path
     schemaBuilder.getDbSchemaBuilder().addMigrationPath(__dirname + '/..');
@@ -87,46 +91,6 @@ export class Email {
       });
   }
 
-  public async sendMessage(to: string,
-                           subject: string,
-                           html: string,
-                           attachments: undefined[] = [],
-                           from?: string,
-                           jobOptions: any = {}): Promise<any> { // todo: jobOptions: use pg-boss interface here
-
-    if (this.isReady) {
-      // Message object
-      const message = { from, to, subject, html };
-
-      try {
-
-        const finalJobOptions = {
-          ... jobOptions,
-          ... this.CONFIG.queue // override methode jobOptions if they interfere
-        };
-
-        // create sendmail job in queue
-        const queue = await this.queueFactory.getQueue();
-        const jobId = await queue.publish(this.queueName, message, finalJobOptions);
-        this.logger.trace('sendMessage.job.creation.success', jobId);
-        return jobId;
-
-      } catch (err) {
-        this.logger.warn('sendMessage.job.creation.error', err);
-        throw err;
-      }
-
-    } else {
-
-      // retry sending message when transport is ready
-      this.eventEmitter.on('transport.ready', async () => {
-        return await this.sendMessage(to, subject, html, attachments, from);
-      });
-
-    }
-
-  }
-
   private async _sendMail(job: any) {
     const message = job.data;
 
@@ -165,6 +129,65 @@ export class Email {
     this.isReady = true;
     this.eventEmitter.emit('transport.ready');
     this.logger.trace('transport.ready');
+
+  }
+
+  public async sendMessage(to: string,
+                           subject: string,
+                           html: string,
+                           text: string = null,
+                           attachments: undefined[] = [],
+                           from?: string,
+                           jobOptions: any = {}): Promise<any> { // todo: jobOptions: use pg-boss interface here
+
+    if (this.isReady) {
+      // Message object
+      const message = { from, to, subject, html, text };
+
+      try {
+
+        const finalJobOptions = {
+          ... jobOptions,
+          ... this.CONFIG.queue // override method jobOptions if they interfere
+        };
+
+        // create sendmail job in queue
+        const queue = await this.queueFactory.getQueue();
+        const jobId = await queue.publish(this.queueName, message, finalJobOptions);
+        this.logger.trace('sendMessage.job.creation.success', jobId);
+        return jobId;
+
+      } catch (err) {
+        this.logger.warn('sendMessage.job.creation.error', err);
+        throw err;
+      }
+
+    } else {
+
+      // retry sending message when transport is ready
+      this.eventEmitter.on('transport.ready', async () => {
+        return await this.sendMessage(to, subject, html, text, attachments, from);
+      });
+
+    }
+
+  }
+
+  public async sendTemplateMail(to: string,
+                                subject: string,
+                                template: any,
+                                text: string = null,
+                                attachments: undefined[] = [],
+                                from?: string,
+                                jobOptions: any = {}): Promise<any> {
+
+    // Generate an HTML email with the provided contents
+    const emailBody = this.mailGenerator.generate(template);
+
+    // Generate the plaintext version of the e-mail (for clients that do not support HTML)
+    const emailText = text || this.mailGenerator.generatePlaintext(emailBody);
+
+    return this.sendMessage(to, subject, emailBody, emailText, attachments, from, jobOptions);
 
   }
 
