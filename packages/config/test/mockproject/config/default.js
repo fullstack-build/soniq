6dd81bbd2741034a39cbcc399c01ee319b9017bf
David Sparkles <david.sparkles@posteo.de>
2019-01-10 10:08:08 +0100

    created mockproject and config ENVIRONTMENT test for config package

---
+++ packages/config/test/mockproject/config/default.js
@@ -0,0 +1,5 @@
+module.exports = {
+  Config: {
+    namespace: 'mock'
+  }
+};
--- packages/config/test/test.ts
+++ packages/config/test/test.ts
@@ -1,6 +1,21 @@
-import ava from "ava";
+import ava, { ExecutionContext } from "ava";
 import { Config } from "../lib/index";
 
-ava("Dummy test", (test) => {
-  test.is("one", "one");
+ava.beforeEach("Set mock project path", (test) => {
+  const projectPath = `${__dirname}/mockproject/`;
+  require.main.filename = `${projectPath}/index.js`;
+
+  test.context = { projectPath };
+});
+
+ava("Construct config ENVIRONMENT", (test: ExecutionContext<{ projectPath: string }>) => {
+  const config = new Config();
+
+  test.is(config.ENVIRONMENT.NODE_ENV, "test");
+  test.is(config.ENVIRONMENT.frameworkVersion, require("../package.json").version);
+  test.is(config.ENVIRONMENT.name, "mockproject");
+  test.is(config.ENVIRONMENT.namespace, "mock");
+  test.is(config.ENVIRONMENT.nodeId.length, 6);
+  test.is(config.ENVIRONMENT.path, test.context.projectPath);
+  test.is(config.ENVIRONMENT.version, "1.0.0");
 });
