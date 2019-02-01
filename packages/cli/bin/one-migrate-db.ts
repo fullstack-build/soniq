f67dea67affb68cbda0e2ef8ce2bd5317c64dc06
David Sparkles <david.sparkles@posteo.de>
2019-02-01 17:31:18 +0100

    cli: migration executable and add command to examples package.json

--- packages/cli/bin/one-migrate-db.ts
+++ packages/cli/bin/one-migrate-db.ts
@@ -1,4 +1,32 @@
 #!/usr/bin/env node
 // tslint:disable:no-console
 
-console.log("Migrate the database.");
+import * as path from "path";
+import * as dotenv from "dotenv";
+
+const projectRootMainFile = `${process.argv[1].split("node_modules/")[0]}index.ts`;
+const projectRootDir = path.dirname(projectRootMainFile);
+console.log(`Assumed ProjectRootMainFile: ${projectRootMainFile}`);
+console.log(`Assumed ProjectRootDir: ${projectRootDir}`);
+console.log();
+
+// Manipulate main file name, so @fullstack-one/config gets the config and we can get the env.
+require.main.filename = projectRootMainFile;
+const dotEnvPath = `${projectRootDir}/.env`;
+dotenv.config(dotEnvPath);
+
+import { Container } from "@fullstack-one/di";
+import { BootLoader } from "@fullstack-one/boot-loader";
+import { AutoMigrate } from "@fullstack-one/auto-migrate";
+
+const $bootLoader: BootLoader = Container.get(BootLoader);
+const $autoMigrate: AutoMigrate = Container.get(AutoMigrate);
+
+console.log();
+console.log("Start booting including migration of db by auto-migrate ...");
+console.log();
+$bootLoader.boot().then(() => {
+  console.log();
+  console.log("Finished booting and db migration.");
+  process.exit();
+});
