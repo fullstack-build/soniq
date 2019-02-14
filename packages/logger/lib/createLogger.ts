d92ba78a1b8ca5d989ed35a74f134d618f070829
David Sparkles <david.sparkles@posteo.de>
2019-02-14 14:09:08 +0100

    logger: use uptodate tracer. simplified.

--- packages/logger/lib/createLogger.ts
+++ packages/logger/lib/createLogger.ts
@@ -7,26 +7,15 @@ export type ILogger = Tracer.Logger;
 export function createLogger(moduleName: string = "root", loggerConfig: any, env: IEnvironment): ILogger {
   const levels = ["trace", "debug", "info", "warn", "error"];
   const loggerName = `${env.namespace}:${env.nodeId}:${moduleName}`;
+  const debugLogger = DebugLogger(loggerName);
 
   const tracerConfig: Tracer.LoggerConfig = {
     level: loggerConfig.minLevel,
     methods: levels,
-    transport: getDebugLoggerTransportFunction(loggerConfig)
+    transport: (logObject: Tracer.LogOutput): void => debugLogger[logObject.title](logObject.output)
   };
 
   const tracerLogger: Tracer.Logger = colorConsole(tracerConfig);
 
   return tracerLogger;
-}
-
-function getDebugLoggerTransportFunction(loggerName: string): Tracer.TransportFunction {
-  const debugLogger = DebugLogger(loggerName);
-  return (logObject: Tracer.LogOutput): void => {
-    // use tracer output to log message to DEBUG
-    debugLogger[logObject.title](logObject.output);
-
-    // adjust for log entries
-    const logArgs = { ...logObject.args };
-    delete logArgs[0];
-  };
 }
