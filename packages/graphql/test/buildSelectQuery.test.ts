cb110ab1243b1786ec820e590d8db59fd2664dd7
David Sparkles <david.sparkles@posteo.de>
2019-02-26 15:15:37 +0100

    graphql: tests for queries with required authentication.

--- packages/graphql/test/buildSelectQuery.test.ts
+++ packages/graphql/test/buildSelectQuery.test.ts
@@ -4,10 +4,11 @@ import QueryBuild from "../lib/getDefaultResolvers/QueryBuilder/QueryBuild";
 import simple from "./queryBuildTestData/simple";
 import filterArguments from "./queryBuildTestData/filterArguments";
 import relationSelect from "./queryBuildTestData/relationSelect";
+import simpleWithAuthentication from "./queryBuildTestData/simpleWithAuthentication";
 
 ava("Simplest SELECT", (test) => {
   const { expected, resolverMeta, dbMeta, query } = simple;
-  const queryBuild = new QueryBuild(resolverMeta, dbMeta, true, Infinity, query);
+  const queryBuild = new QueryBuild(resolverMeta, dbMeta, false, Infinity, query);
   const actual = queryBuild.getBuildObject();
   actual.costTree = null;
 
@@ -16,7 +17,7 @@ ava("Simplest SELECT", (test) => {
 
 ava("SELECT with WHERE, ORDER BY, LIMIT, OFFSET", (test) => {
   const { expected, resolverMeta, dbMeta, query } = filterArguments;
-  const queryBuild = new QueryBuild(resolverMeta, dbMeta, true, Infinity, query);
+  const queryBuild = new QueryBuild(resolverMeta, dbMeta, false, Infinity, query);
   const actual = queryBuild.getBuildObject();
   actual.costTree = null;
 
@@ -25,6 +26,15 @@ ava("SELECT with WHERE, ORDER BY, LIMIT, OFFSET", (test) => {
 
 ava("SELECT with sub relation (n+1 problem solved by a single query)", (test) => {
   const { expected, resolverMeta, dbMeta, query } = relationSelect;
+  const queryBuild = new QueryBuild(resolverMeta, dbMeta, false, Infinity, query);
+  const actual = queryBuild.getBuildObject();
+  actual.costTree = null;
+
+  test.deepEqual(actual, expected);
+});
+
+ava("SELECT with authentication", (test) => {
+  const { expected, resolverMeta, dbMeta, query } = simpleWithAuthentication;
   const queryBuild = new QueryBuild(resolverMeta, dbMeta, true, Infinity, query);
   const actual = queryBuild.getBuildObject();
   actual.costTree = null;
@@ -32,9 +42,16 @@ ava("SELECT with sub relation (n+1 problem solved by a single query)", (test) =>
   test.deepEqual(actual, expected);
 });
 
-ava.todo("SELECT with authentication");
-
-ava.todo("SELECT with required but missing authentication");
+ava("SELECT with required but missing authentication", (test) => {
+  const { resolverMeta, dbMeta, query } = simpleWithAuthentication;
+  try {
+    const queryBuild = new QueryBuild(resolverMeta, dbMeta, false, Infinity, query);
+    queryBuild.getBuildObject();
+    test.fail();
+  } catch (err) {
+    test.pass();
+  }
+});
 
 ava.todo("Cost tree");
 
