2374d660d64f93b94332c9e02074887aa057fa0c
David Sparkles <david.sparkles@posteo.de>
2019-02-27 14:51:52 +0100

    graphql: added tests for mutation building

--- packages/graphql/test/buildMutationQuery.test.ts
+++ packages/graphql/test/buildMutationQuery.test.ts
@@ -1,7 +1,28 @@
 import ava from "ava";
+import resolveCreateMutation from "../lib/getDefaultResolvers/MutationBuilder/resolveCreateMutation";
+import resolveUpdateMutation from "../lib/getDefaultResolvers/MutationBuilder/resolveUpdateMutation";
+import resolveDeleteMutation from "../lib/getDefaultResolvers/MutationBuilder/resolveDeleteMutation";
+import createTestData from "./mutationBuildTestData/createTestData";
+import updateTestData from "./mutationBuildTestData/updateTestData";
+import deleteTestData from "./mutationBuildTestData/deleteTestData";
 
-ava.todo("CREATE query");
+ava("CREATE query", (test) => {
+  const { expected, query, mutation } = createTestData;
+  const actual = resolveCreateMutation(query, mutation);
 
-ava.todo("UPDATE query");
+  test.deepEqual(actual, expected);
+});
 
-ava.todo("DELETE query");
+ava("UPDATE query", (test) => {
+  const { expected, query, mutation } = updateTestData;
+  const actual = resolveUpdateMutation(query, mutation);
+
+  test.deepEqual(actual, expected);
+});
+
+ava("DELETE query", (test) => {
+  const { expected, query, mutation } = deleteTestData;
+  const actual = resolveDeleteMutation(query, mutation);
+
+  test.deepEqual(actual, expected);
+});
