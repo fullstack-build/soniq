2455e1edd5cb30d7bc3e20b3848546cf72fa47a9
dustin-H <dustin.hoffner@gmail.com>
2019-03-28 20:56:59 +0100

    Create Table SQL Auth2

---
+++ packages/auth/sql/20000/meta.Auth.insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO "_meta"."Auth"("key","value")
+VALUES
+(E'admin_token_secret',E'boss'),
+(E'auth_factor_providers',E'password:email'),
+(E'hash_bf_iter_count',E'6'),
+(E'hash_secret',E'gehtdichnixan'),
+(E'login_token_bf_iter_count',E'4'),
+(E'login_token_max_age_in_seconds',E'1209600'),
+(E'login_token_secret',E'geheim'),
+(E'refresh_token_bf_iter_count',E'6'),
+(E'refresh_token_secret',E'ThisIsSparta'),
+(E'transaction_token_max_age_in_seconds',E'86400'),
+(E'transaction_token_secret',E'$2a$04$xzDLBUt4RbSktPsKUUjuneD5pOkGla8KQv0eexC4f6KQSFLMqnDBu'),
+(E'transaction_token_timestamp',E'1553791834');
