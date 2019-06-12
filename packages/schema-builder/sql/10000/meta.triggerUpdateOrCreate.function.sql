60e29ed2b15dd0302f8e459a693784e7fb3afaa5
David Sparkles <david.sparkles@posteo.de>
2019-06-12 10:13:05 +0200

    orm: finalized initial db migration

--- packages/schema-builder/sql/10000/meta.triggerUpdateOrCreate.function.sql
+++
@@ -1,4 +0,0 @@
-CREATE OR REPLACE FUNCTION _meta.triggerUpdateOrCreate() RETURNS trigger AS $$
-	NEW['updatedAt'] = plv8.execute( 'SELECT NOW() AT TIME ZONE \'UTC\' now;')[0].now;
-	return NEW;
-$$ LANGUAGE plv8 STABLE;
