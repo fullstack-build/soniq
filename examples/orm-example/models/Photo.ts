bff4842b248b1c7808cabd95e11eaa2ba5f942da
David Sparkles <david.sparkles@posteo.de>
2019-06-14 18:05:46 +0200

    orm: execute fullstack migrations on boot

--- examples/orm-example/models/Photo.ts
+++ examples/orm-example/models/Photo.ts
@@ -1,11 +1,10 @@
 import * as typeorm from "typeorm";
 import { createColumnDecorator, createColumnDecoratorFactory, Entity, PrimaryGeneratedColumn, Column } from "@fullstack-one/db";
-import User from "./User";
 
 const myDecorator = createColumnDecorator({ directive: "@myDirective" });
 const myDecoratorFactory = createColumnDecoratorFactory<{ max: number }>({ getDirective: ({ max }) => `@myParamDirective(max: ${max})` });
 
-@Entity({ schema: "other" })
+@Entity({ schema: "public" })
 export default class Photo extends typeorm.BaseEntity {
   @PrimaryGeneratedColumn({ comment: "This is a very important id column." })
   public id: number;
---
+++ packages/db/lib/migrations/index.ts
@@ -0,0 +1,9 @@
+export { Extensions1560087843421 } from "./1560087843421-Extensions";
+export { Schema1560153165901 } from "./1560153165901-Schema";
+export { MetaInfoTable1560153367583 } from "./1560153367583-MetaInfoTable";
+export { MetaMigrationsTable1560153367683 } from "./1560153367683-MetaMigrationsTable";
+export { MetaPlv8JsModulesTable1560160652405 } from "./1560160652405-MetaPlv8JsModulesTable";
+export { MetaFunctions1560161087095 } from "./1560161087095-MetaFunctions";
+export { SetPlv8StartProc1560162945204 } from "./1560162945204-SetPlv8StartProc";
+export { MetaInserts1560163271393 } from "./1560163271393-MetaInserts";
+export { MetaTypes1560164600721 } from "./1560164600721-MetaTypes";
