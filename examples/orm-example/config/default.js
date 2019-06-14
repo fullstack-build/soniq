bf0473930c74463c61937724e5b376bd9f40c5f9
David Sparkles <david.sparkles@posteo.de>
2019-06-14 10:39:12 +0200

    orm-example: pretty config

--- examples/orm-example/config/default.js
+++ examples/orm-example/config/default.js
@@ -38,14 +38,14 @@ module.exports = {
     },
     orm: {
       connection: {
-        "type":         process.env.TYPEORM_CONNECTION,
-        "host":         process.env.TYPEORM_HOST,
-        "port":         process.env.TYPEORM_PORT,
-        "username":     process.env.TYPEORM_USERNAME,
-        "password":     process.env.TYPEORM_PASSWORD,
-        "database":     process.env.TYPEORM_DATABASE,
-        "synchronize":  process.env.TYPEORM_SYNCHRONIZE === "true",
-        "logging":      process.env.TYPEORM_LOGGING === "true"
+        type:         process.env.TYPEORM_CONNECTION,
+        host:         process.env.TYPEORM_HOST,
+        port:         process.env.TYPEORM_PORT,
+        username:     process.env.TYPEORM_USERNAME,
+        password:     process.env.TYPEORM_PASSWORD,
+        database:     process.env.TYPEORM_DATABASE,
+        synchronize:  process.env.TYPEORM_SYNCHRONIZE === "true",
+        logging:      process.env.TYPEORM_LOGGING === "true"
       },
     }
   },
