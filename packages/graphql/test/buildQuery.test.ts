78816e8297b4d812ac1f00a93df01d05d5133a35
dustin-H <dustin.hoffner@gmail.com>
2019-06-25 23:48:36 +0200

    Enable Tests again due to recent workaround for #82

--- packages/graphql/test/buildQuery.test.ts
+++ packages/graphql/test/buildQuery.test.ts
@@ -7,61 +7,58 @@ import relationToSingleSelect from "./queryBuildTestData/relationToSingleSelect"
 import relationToManySelect from "./queryBuildTestData/relationToManySelect";
 import simpleWithAuthentication from "./queryBuildTestData/simpleWithAuthentication";
 
-// TODO: Does not work because the LoggerFactory is somewhere injected and Config does not find a default.js
-// https://github.com/fullstack-build/fullstack-one/issues/82
-
 ava("Simplest SELECT", (test) => {
-  //   const { expected, resolverMeta, dbMeta, query } = simple;
-  //   const queryBuild = new QueryBuild(resolverMeta, dbMeta, false, Infinity, query);
-  //   const actual = queryBuild.getBuildObject();
-  //   actual.costTree = null;
-  //   test.deepEqual(actual, expected);
+  const { expected, resolverMeta, dbMeta, query } = simple;
+  const queryBuild = new QueryBuild(resolverMeta, dbMeta, false, Infinity, query);
+  const actual = queryBuild.getBuildObject();
+  actual.costTree = null;
+  test.deepEqual(actual, expected);
   test.is(false, false);
 });
 
-// ava("SELECT with WHERE, ORDER BY, LIMIT, OFFSET", (test) => {
-//   const { expected, resolverMeta, dbMeta, query } = filterArguments;
-//   const queryBuild = new QueryBuild(resolverMeta, dbMeta, false, Infinity, query);
-//   const actual = queryBuild.getBuildObject();
-//   actual.costTree = null;
+ava("SELECT with WHERE, ORDER BY, LIMIT, OFFSET", (test) => {
+  const { expected, resolverMeta, dbMeta, query } = filterArguments;
+  const queryBuild = new QueryBuild(resolverMeta, dbMeta, false, Infinity, query);
+  const actual = queryBuild.getBuildObject();
+  actual.costTree = null;
 
-//   test.deepEqual(actual, expected);
-// });
+  test.deepEqual(actual, expected);
+});
 
-// ava("SELECT with sub relation to Single", (test) => {
-//   const { expected, resolverMeta, dbMeta, query } = relationToSingleSelect;
-//   const queryBuild = new QueryBuild(resolverMeta, dbMeta, false, Infinity, query);
-//   const actual = queryBuild.getBuildObject();
-//   actual.costTree = null;
+ava("SELECT with sub relation to Single", (test) => {
+  const { expected, resolverMeta, dbMeta, query } = relationToSingleSelect;
+  const queryBuild = new QueryBuild(resolverMeta, dbMeta, false, Infinity, query);
+  const actual = queryBuild.getBuildObject();
+  actual.costTree = null;
 
-//   test.deepEqual(actual, expected);
-// });
+  test.deepEqual(actual, expected);
+});
 
-// ava("SELECT with sub relation to Many", (test) => {
-//   const { expected, resolverMeta, dbMeta, query } = relationToManySelect;
-//   const queryBuild = new QueryBuild(resolverMeta, dbMeta, false, Infinity, query);
-//   const actual = queryBuild.getBuildObject();
-//   actual.costTree = null;
+ava("SELECT with sub relation to Many", (test) => {
+  const { expected, resolverMeta, dbMeta, query } = relationToManySelect;
+  const queryBuild = new QueryBuild(resolverMeta, dbMeta, false, Infinity, query);
+  const actual = queryBuild.getBuildObject();
+  actual.costTree = null;
 
-//   test.deepEqual(actual, expected);
-// });
+  test.deepEqual(actual, expected);
+});
 
-// ava("SELECT with authentication", (test) => {
-//   const { expected, resolverMeta, dbMeta, query } = simpleWithAuthentication;
-//   const queryBuild = new QueryBuild(resolverMeta, dbMeta, true, Infinity, query);
-//   const actual = queryBuild.getBuildObject();
-//   actual.costTree = null;
+ava("SELECT with authentication", (test) => {
+  const { expected, resolverMeta, dbMeta, query } = simpleWithAuthentication;
+  const queryBuild = new QueryBuild(resolverMeta, dbMeta, true, Infinity, query);
+  const actual = queryBuild.getBuildObject();
+  actual.costTree = null;
 
-//   test.deepEqual(actual, expected);
-// });
+  test.deepEqual(actual, expected);
+});
 
-// ava("SELECT with required but missing authentication", (test) => {
-//   const { resolverMeta, dbMeta, query } = simpleWithAuthentication;
-//   try {
-//     const queryBuild = new QueryBuild(resolverMeta, dbMeta, false, Infinity, query);
-//     queryBuild.getBuildObject();
-//     test.fail();
-//   } catch (err) {
-//     test.pass();
-//   }
-// });
+ava("SELECT with required but missing authentication", (test) => {
+  const { resolverMeta, dbMeta, query } = simpleWithAuthentication;
+  try {
+    const queryBuild = new QueryBuild(resolverMeta, dbMeta, false, Infinity, query);
+    queryBuild.getBuildObject();
+    test.fail();
+  } catch (err) {
+    test.pass();
+  }
+});
