99fc95e6ef24ad4c973df71484388645678573a4
David Sparkles <david.sparkles@posteo.de>
2019-07-01 16:40:12 +0200

    file-storage: adjusted migration directory name

--- packages/file-storage/lib/migrations/1561973614863-FileStorage-Functions.ts
+++ packages/file-storage/lib/migrations/1561973614863-FileStorage-Functions.ts
@@ -12,10 +12,10 @@ export class FileStorageFunctions1561973614863 implements MigrationInterface {
 }
 
 function loadAuthFunctionsSql(): string {
-  const filenames = readdirSync(`${__dirname}/../../res/file-storage-functions-2`);
+  const filenames = readdirSync(`${__dirname}/../../res/1561973614863-file-storage-functions`);
   return filenames
     .map((filename) => {
-      return readFileSync(`${__dirname}/../../res/file-storage-functions-2/${String(filename)}`, { encoding: "utf-8" }).toString();
+      return readFileSync(`${__dirname}/../../res/1561973614863-file-storage-functions/${String(filename)}`, { encoding: "utf-8" }).toString();
     })
     .join("\n\n");
 }
