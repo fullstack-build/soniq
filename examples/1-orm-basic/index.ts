54437ba75c6af426434f18112da7ef8fa7adef0c
Eugene Terehov <opensource@terehov.de>
2019-07-09 13:59:15 +0200

    Example: 1-orm-basic: cleaned up

--- examples/1-orm-basic/index.ts
+++ examples/1-orm-basic/index.ts
@@ -16,23 +16,10 @@ import Task from "./models/Task";
 (async () => {
   await $one.boot();
 
-  const queryRunner1 = $orm.createQueryRunner();
-  await queryRunner1.connect();
-
-  // const result = await queryRunner1.query(`SELECT * FROM "Task";`);
-  // console.log(`result: ${JSON.stringify(result)}`);
-  // const result2 = await queryRunner1.query(`INSERT INTO public."Task" (title) VALUES ('Test title');`);
-  // console.log(`result2: ${JSON.stringify(result2)}`);
-
-  await queryRunner1.release();
-
   const task = new Task();
   task.title = "Catch the flight";
   await task.save();
   console.log("Task has been saved");
-
-  const queryRunner = $orm.getConnection().createQueryRunner();
-  await queryRunner.connect();
 
   const tasks = await Task.find();
   console.log("Loaded tasks: ", JSON.stringify(tasks, null, 2));
