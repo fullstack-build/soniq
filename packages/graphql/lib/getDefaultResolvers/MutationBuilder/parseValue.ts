a409099d000c3076bce6ea0c8b9d86845d14fa3d
dustin-H <dustin.hoffner@gmail.com>
2019-08-17 17:31:58 +0200

    Pass null as null to default mutation queries insert and update not as string

--- packages/graphql/lib/getDefaultResolvers/MutationBuilder/parseValue.ts
+++ packages/graphql/lib/getDefaultResolvers/MutationBuilder/parseValue.ts
@@ -1,6 +1,11 @@
 import { ReturnIdHandler } from "../../ReturnIdHandler";
 
-export default function parseValue(value: any, returnIdHandler: ReturnIdHandler): string {
-  if (value != null && typeof value === "object") return JSON.stringify(value);
+export default function parseValue(value: any, returnIdHandler: ReturnIdHandler): string | null {
+  if (value != null && typeof value === "object") {
+    return JSON.stringify(value);
+  }
+  if (value == null) {
+    return null;
+  }
   return returnIdHandler.getReturnId(`${value}`);
 }
