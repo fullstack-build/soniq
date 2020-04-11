7031e1cc35b09151efcebd25c1d1cca4fc2ff33d
Eugene Terehov <opensource@terehov.de>
2020-04-11 16:08:25 +0800

    Set up of first module (core) to work with DI, ts-node during dev and build version during prod

---
+++ modules/core/dist/index.d.ts
@@ -0,0 +1,6 @@
+import "reflect-metadata";
+import { Service, Container } from "typedi";
+export { Service, Container };
+export declare class Core {
+    constructor();
+}
--- modules/core/src/index.ts
+++ modules/core/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World.");
+import "reflect-metadata";
+import {Service, Container} from "typedi";
+export {Service, Container};
+
+@Service("@fullstack-one/core")
+export class Core {
+
+  constructor() {
+    process.stdout.write(
+      `  ┌─┐┬ ┬┬  ┬  ┌─┐┌┬┐┌─┐┌─┐┬┌─ ┌─┐┌┐┌┌─┐\n  ├┤ │ ││  │  └─┐ │ ├─┤│  ├┴┐ │ ││││├┤\n  └  └─┘┴─┘┴─┘└─┘ ┴ ┴ ┴└─┘┴ ┴o└─┘┘└┘└─┘\n\n`
+    );
+    //process.stdout.write(`${JSON.stringify(this.ENVIRONMENT, null, 2)}\n`);
+    process.stdout.write("____________________________________\n");
+  }
+
+}
