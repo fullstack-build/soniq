2e746ad2e830b7a0d10ddb93dad566accc9e27ef
Eugene Terehov <opensource@terehov.de>
2020-04-11 20:18:40 +0800

    WIP: Settign up pre-commit hook and lint-staged

--- modules/core/dist/index.d.ts
+++ modules/core/dist/index.d.ts
@@ -2,5 +2,5 @@ import "reflect-metadata";
 import { Service, Container } from "typedi";
 export { Service, Container };
 export declare class Core {
-    constructor();
+    private constructor();
 }
--- modules/core/src/index.ts
+++ modules/core/src/index.ts
@@ -1,16 +1,14 @@
 import "reflect-metadata";
-import {Service, Container} from "typedi";
-export {Service, Container};
+import { Service, Container } from "typedi";
+export { Service, Container };
 
 @Service("@fullstack-one/core")
 export class Core {
-
-  constructor() {
+  private constructor() {
     process.stdout.write(
       `  ┌─┐┬ ┬┬  ┬  ┌─┐┌┬┐┌─┐┌─┐┬┌─ ┌─┐┌┐┌┌─┐\n  ├┤ │ ││  │  └─┐ │ ├─┤│  ├┴┐ │ ││││├┤\n  └  └─┘┴─┘┴─┘└─┘ ┴ ┴ ┴└─┘┴ ┴o└─┘┘└┘└─┘\n\n`
     );
     //process.stdout.write(`${JSON.stringify(this.ENVIRONMENT, null, 2)}\n`);
     process.stdout.write("____________________________________\n");
   }
-
 }
