d474758b48db53597f9d7904a7780e8ce8f5f953
Eugene Terehov <opensource@terehov.de>
2020-04-11 15:57:01 +0800

    Added TypeDi to core and conigured TypeScript execution during development and JS when using

--- examples/01/dist/index.js
+++ examples/01/dist/index.js
@@ -1,2 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@fullstack-one/core");
 console.log("Hello World.");
+const core = core_1.Container.get("@fullstack-one/core");
--- examples/01/src/index.ts
+++ examples/01/src/index.ts
@@ -1,1 +1,5 @@
+import {Container} from "@fullstack-one/core";
+
 console.log("Hello World.");
+
+const core = Container.get("@fullstack-one/core");
