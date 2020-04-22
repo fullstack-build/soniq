8af143199baf425d8f66e76cf11079680a96eb10
Eugene Terehov <opensource@terehov.de>
2020-04-22 13:00:26 +0800

    Updated tsconfig and rebuild

--- modules/Core/dist/index.d.ts
+++ modules/Core/dist/index.d.ts
@@ -1,17 +1,19 @@
 import "reflect-metadata";
 import { Service, Container, ContainerInstance, Inject, InjectMany } from "typedi";
 export { Service, Container, ContainerInstance, Inject, InjectMany };
-import { Logger } from "./Logger";
+import { Logger } from "tslog";
 import { ConfigManager, IEnvironment } from "./ConfigManager";
 export { IEnvironment };
 export { Logger };
 export declare class Core {
-    private readonly className;
-    private readonly bootLoader;
+    private readonly _className;
+    private readonly _bootLoader;
+    private readonly _logger;
     readonly configManager: ConfigManager | undefined;
     readonly ENVIRONMENT: IEnvironment;
-    private readonly logger;
-    private constructor();
-    private drawCliArt;
+    constructor();
+    private _drawCliArt;
     boot(): Promise<void>;
+    getLogger(name?: string, minLevel?: number, exposeStack?: boolean): Logger;
 }
+//# sourceMappingURL=index.d.ts.map
