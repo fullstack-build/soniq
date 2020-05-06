b06438defb6830d61889c5e98b3cbd0ee050129f
dustin-H <dustin.hoffner@gmail.com>
2020-05-06 16:52:04 +0200

    Update packages to merge

--- eslintrc.js
+++ eslintrc.js
@@ -1,5 +1,5 @@
 // This is a workaround for https://github.com/eslint/eslint/issues/3458
-require("@rushstack/eslint-config/patch-eslint6");
+require("./common/temp/node_modules/@rushstack/eslint-config/patch-eslint6");
 
 module.exports = {
   plugins: ["prettier"],
@@ -7,7 +7,7 @@ module.exports = {
     "prettier/prettier": "error",
     "eqeqeq": [2, "smart"]
   },
-  extends: ["@rushstack/eslint-config", "plugin:prettier/recommended"],
+  extends: ["./common/temp/node_modules/@rushstack/eslint-config", "plugin:prettier/recommended"],
   "ignorePatterns": ["node_modules/", "dist/", "tests", "*.test.ts", "_v1", "_v1_mig3"],
   env: {
     node: true
