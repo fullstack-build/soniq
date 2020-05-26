a75d7bdf6660ead328de439a6251d7b3b51ea7d7
dustin-H <dustin.hoffner@gmail.com>
2020-05-26 22:37:26 +0200

    Update example - change exports and add defaultExpression

--- examples/02/src/schema/tables/Post/index.ts
+++ examples/02/src/schema/tables/Post/index.ts
@@ -4,14 +4,16 @@ import { Table, GenericColumn, IdColumn, ManyToOneColumn } from "@soniq/graphql"
 import { anyone } from "../../expressions";
 import { user } from "../User";
 
-export const post = new Table("Post", "soniq");
+const post = new Table("Post", "soniq");
 
-export const postId = new IdColumn();
-export const title = new GenericColumn("title", "text");
-export const content = new GenericColumn("content", "text");
-export const owner = new ManyToOneColumn("owner", () => user);
+const postId = new IdColumn();
+const title = new GenericColumn("title", "text");
+const content = new GenericColumn("content", "text");
+const owner = new ManyToOneColumn("owner", () => user);
 
 post.addColumn(0, postId, anyone);
 post.addColumn(1, title, anyone);
 post.addColumn(2, content, anyone);
 post.addColumn(3, owner, anyone);
+
+export { post, postId, title, content, owner };
