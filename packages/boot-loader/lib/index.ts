9e51f3363e3da4b09db5b7459632ed96a9247a42
Eugene Terehov <opensource@terehov.de>
2020-06-23 00:34:02 +0200

    Remove Logger from tests

--- packages/boot-loader/lib/index.ts
+++ packages/boot-loader/lib/index.ts
@@ -24,12 +24,6 @@ export class BootLoader {
   private bootFunctionObjects: IBootFunctionObject[] = [];
   private afterBootFunctionObjects: IAfterBootFunctionObject[] = [];
 
-  private readonly logger: Logger;
-
-  constructor(@Inject((type) => LoggerFactory) loggerFactory: LoggerFactory) {
-    this.logger = loggerFactory.create(this.constructor.name);
-  }
-
   public getBootState(): EBootState {
     return this.state;
   }
@@ -43,12 +37,10 @@ export class BootLoader {
   }
 
   public addBootFunction(name: string, fn: TBootFuntion): void {
-    this.logger.debug("addBootFunction", name);
     this.bootFunctionObjects.push({ name, fn });
   }
 
   public onBootReady(name: string, fn: TBootFuntion): void | Promise<void> {
-    this.logger.debug("onBootReady", name);
     if (this.state === EBootState.Finished) {
       return fn();
     }
@@ -65,18 +57,13 @@ export class BootLoader {
     this.state = EBootState.Booting;
     try {
       for (const fnObj of this.bootFunctionObjects) {
-        this.logger.debug("boot.bootFunctions.start", fnObj.name);
         await fnObj.fn(this);
-        this.logger.debug("boot.bootFunctions.end", fnObj.name);
       }
       this.state = EBootState.Finished;
       for (const fnObj of this.afterBootFunctionObjects) {
-        this.logger.debug("boot.afterBootFunctions.start", fnObj.name);
         fnObj.fn(this);
-        this.logger.debug("boot.afterBootFunctions.start", fnObj.name);
       }
     } catch (err) {
-      this.logger.error(`BootLoader.boot.error.caught: ${err}\n`);
       throw err;
     }
   }
