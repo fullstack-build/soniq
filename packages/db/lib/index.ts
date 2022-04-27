f6f130268372d3b32f360b8a63aec85d8b8194c1
Eugene Terehov <opensource@terehov.de>
2022-04-27 17:04:00 +0100

    Remove DB connection resize and update dependencies

--- packages/db/lib/index.ts
+++ packages/db/lib/index.ts
@@ -10,7 +10,6 @@ import { GracefulShutdown } from "@fullstack-one/graceful-shutdown";
 import { EventEmitter } from "@fullstack-one/events";
 import createTriggers from "./createTriggers";
 import * as dbMigrationsObject from "./migrations";
-import getClientManager, { IClientManager } from "./getClientManager";
 import gracefullyRemoveConnection from "./gracefullyRemoveConnection";
 import * as modelMeta from "./model-meta";
 import { IDbConfig } from "./IDbConfig";
@@ -35,7 +34,6 @@ export class ORM {
   private readonly environment: IEnvironment;
   private readonly applicationNamePrefix: string;
   private readonly applicationName: string;
-  private readonly clientManager: IClientManager;
   private readonly migrations: Array<new () => typeorm.MigrationInterface> = [];
   private readonly entities: Array<new () => any> = [];
   private readonly subscribers: Array<new () => typeorm.EntitySubscriberInterface> = [];
@@ -55,8 +53,6 @@ export class ORM {
     this.applicationNamePrefix = `${environment.namespace}_orm_`;
     this.applicationName = `${this.applicationNamePrefix}${environment.nodeId}`;
 
-    this.clientManager = getClientManager(environment.nodeId, this.adjustORMPoolSize.bind(this));
-
     if (this.config.synchronize) {
       this.addMigrations(Object.values(dbMigrationsObject));
     }
@@ -73,7 +69,6 @@ export class ORM {
     if (this.config.synchronize) {
       await createTriggers(this, this.logger);
     }
-    await this.clientManager.start();
     await this.eventEmitter.emit("db.orm.pool.connect.success", this.applicationName);
   }
 
@@ -121,27 +116,8 @@ export class ORM {
     this.logger.debug("db.orm.pool.connect.success", `TypeORM pool created (min: ${connectionOptions.extra.min} / max: ${max})`);
   }
 
-  private async adjustORMPoolSize(numberOfConnectedNodes: number): Promise<void> {
-    const connectionsPerInstance: number = Math.floor(this.config.globalMax / numberOfConnectedNodes);
-    this.logger.debug(
-      `adjustORMPoolSize with ${connectionsPerInstance} connections per instance for ${numberOfConnectedNodes} connected nodes` +
-        `and a global maximum of ${this.config.globalMax}.`
-    );
-
-    if (typeorm.getConnection() != null) {
-      gracefullyRemoveConnection(typeorm.getConnection()).then(() => {
-        this.logger.debug("db.orm.old.connection.removed");
-      });
-    }
-
-    await this.createConnection(connectionsPerInstance);
-  }
-
   private async end(): Promise<void> {
     this.logger.debug("Postgres ORM pool ending initiated");
-
-    this.eventEmitter.emit("db.orm.pool.end.start", this.applicationName);
-    await this.clientManager.stop();
 
     try {
       await typeorm.getConnection().close();
